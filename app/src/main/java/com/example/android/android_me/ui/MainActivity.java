e3dc93ff1f08b9792793c4e3c4c94b7393f4e736
kwiecien <k.d.kwiecinski@gmail.com>
2018-04-08 17:24:15 +0200

    Add Master Detail flow

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -1,31 +1,38 @@
 /*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.android_me.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
 
 // This activity is responsible for displaying the master list of all images
 // Implement the MasterListFragment callback, OnImageClickListener
-public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener{
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener {
 
+
+    private int mHead;
+    private int mBody;
+    private int mLegs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +42,42 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
     }
 
     // Define the behavior for onImageSelected
+    @Override
     public void onImageSelected(int position) {
         // Create a Toast that displays the position that was clicked
         Toast.makeText(this, "Position clicked = " + position, Toast.LENGTH_SHORT).show();
 
-        // TODO (2) Based on where a user has clicked, store the selected list index for the head, body, and leg BodyPartFragments
+        int bodyPart = position / 12;
+        int index = position - 12 * bodyPart;
+        switch (bodyPart) {
+            case 0:
+                mHead = index;
+                break;
+            case 1:
+                mBody = index;
+                break;
+            case 2:
+                mLegs = index;
+                break;
+            default:
+        }
 
-        // TODO (3) Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
 
-        // TODO (4) Get a reference to the "Next" button and launch the intent when this button is clicked
+        Bundle bundle = new Bundle();
+        bundle.putInt("mHead", mHead);
+        bundle.putInt("mBody", mBody);
+        bundle.putInt("mLegs", mLegs);
+
+        final Intent intent = new Intent(this, AndroidMeActivity.class);
+        intent.putExtra("fullBody", bundle);
+
+        Button nextButton = (Button) findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
 
     }
 
